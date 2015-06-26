e5f0c7f825347d397116bcd3e9332ccd47ea4a5a
Will Greenberg <will@endlessm.com>
2015-06-26 15:16:32 -0700

    Move LZ77 specific stuff to its own files

--- src/lz77.js
+++ src/lz77.js
@@ -1,9 +1,10 @@
+var LZ77ISA = [Interpreter.PRINT, Interpreter.REPEAT];
 function setup() {
     var inbox = document.getElementById('input');
     var outbox = document.getElementById('output');
     inbox.addEventListener('input', function () {
         try {
-            var program = Interpreter.parse(inbox.value);
+            var program = Interpreter.parse(inbox.value, LZ77ISA);
             var output = Interpreter.run(program);
             outbox.disabled = false;
             outbox.value = output;
